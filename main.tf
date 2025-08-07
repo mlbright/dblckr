a00fcc5c999927ae8af79804019af7d875567ea8
Martin-Louis Bright <mlbright@gmail.com>
2025-08-07 15:14:49 -0400

    wip

--- main.tf
+++ main.tf
@@ -108,9 +108,9 @@ resource "aws_autoscaling_group" "adblocker" {
 
   mixed_instances_policy {
     instances_distribution {
-      on_demand_allocation_strategy            = "prioritized"
+      on_demand_allocation_strategy            = "lowest-price"
       on_demand_base_capacity                  = 0
-      on_demand_percentage_above_base_capacity = 100
+      on_demand_percentage_above_base_capacity = 0
       spot_allocation_strategy                 = "price-capacity-optimized"
     }
 
